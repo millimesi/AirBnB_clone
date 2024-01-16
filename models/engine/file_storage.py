298b69feb65ee46c8acf6e72a402b18fb9fdf923
Mmadineo <mmadineo.seleke@gmail.com>
2024-01-16 01:58:38 -0800

    file_storage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,8 +16,9 @@ from models.review import Review
 
 class FileStorage():
     """ serialization and Desrialization"""
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self):
+    self.__file_path = "file.json"
+    self.__objects = {}
 
     def all(self):
         """ returns the dictionary __objects"""
